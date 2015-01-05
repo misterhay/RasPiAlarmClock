39dcb5d2d1260bbad2c31b0b0864a06cf71fbcc6
David Hay <misterhay@gmail.com>
2015-01-05 07:11:19 -0700

    added some notes about loops and conditionals

--- wakeup.sh
+++ wakeup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # set this to run at the day and time you want to wake up
+# loop all of this (with a 1 minute pause at the end) to be like a "snooze button" if the switch is not turned off
+# try all of this, and catch error by playing prerecorded "Good morning, it appears that we don't have Internet access, but you need to get up anyway"
+# if GPIO pin (switch) is on then do all of this, else skip (and stay silent)
 dayName=`date +%A`
 month=`date +%B`
 dayNumber=`date +%d`
@@ -10,7 +13,7 @@ highTemp=`echo $temperatures | awk -F'"' '{print $6}`
 symbol=`echo $weather | grep symbol`
 conditions=`echo $symbol | awk -F'"' '{print $4}`
 # build what we'll have it say
-# Good Morning. Today is $dayName, $month $dayNumber+st/nd/rd/th. The forecast calls for $conditions and $highTemp degrees.
+# Good morning. Today is $dayName, $month $dayNumber+st/nd/rd/th. The forecast calls for $conditions and $highTemp degrees.
 speakThis=
 # call the mpg123 program to speak the text (using a Google Translate URL)
 mpg123 -q "http://translate.google.com/translate_tts?tl=en&q=Today+is+Monday%2C+January+5th"
